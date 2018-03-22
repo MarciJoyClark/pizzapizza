99e98525ef36a81ccbb299aad73f3730e69a50ae
MarciJoyClark <33448580+MarciJoyClark@users.noreply.github.com>
2018-03-22 16:02:41 -0500

    initial upload
    Open pizza3.html.

---
+++ pizza/index.js
@@ -0,0 +1,40 @@
+$('.block:first')
+  .addClass('current first')
+  .append('<span class="btn" id="next">Lets go!</span>');
+  
+$('.block:last')
+  .addClass('empty last')
+  .append('<span class="btn" id="prev">Prev</span>') 
+  
+$('.block:nth-last-child(2)')
+  .addClass('current nth-last-child2')
+  .append('<span clas="btn" id="nth-last-child2"><i class="fa fa-rocket"></i>)</span>')
+  .append('<span class="btn" id="prev">Prev</span>')
+  
+$('.block').not(':first, :last, :nth-last-child2')
+  .addClass('empty')
+  .append('<span class="btn" id="next">Next<i class="fa fa-hand-o-right"></i></span>')
+  .append('<span class="btn" id="prev">Prev</span>');
+
+$('.btn').click( function() {
+  var dir = event.target.id;
+  var current = $('.current'),
+      next = $('.current').next('div'),
+      prev = $('.current').prev('div'),
+      nth-last-child2 = $('.current').nth-last-child2('div');
+	  
+  if (dir == 'next') {
+    current.removeClass('current').addClass('filled');
+    next.removeClass('empty').addClass('current');
+  }
+  if (dir == 'prev') {
+    current.removeClass('current').addClass('empty');
+    prev.removeClass('filled').addClass('current');
+  }
+  if (dir == 'nth-last-child2').click( function getReceipt() {
+    current.removeClass('current').addClass('filled');
+    prev.removeClass('filled').addClass('current');
+  }
+});
+
+$(':input[required]').closest('.block').addClass('mandatory');
